a4a0cbcca6d65e81c378bd2c63e132d3b828fb10
Zachary Goodman <znote89@gmail.com>
2021-08-22 20:59:44 -0400

    commit tree 8707bab46a7eb13376f2fea61d4008ee3c0b7b12

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
   get '/' do
     { message: "Hello world" }.to_json
   end
 
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  get '/games/:id' do
+    games = Game.find(params[:id])
+    games.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+  end
 end
